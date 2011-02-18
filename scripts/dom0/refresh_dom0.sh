e523471c08f043b8b0dfb6c67f36028e3294c604
Chris Benninger <chrisbenninger@gmail.com>
2011-02-18 17:56:35 +0000

    asd

--- scripts/dom0/refresh_dom0.sh
+++ scripts/dom0/refresh_dom0.sh
@@ -3,8 +3,8 @@
 #/home/chris/workspace/malware/drivers/xen/monitor/monitor.ko \
 
 scp chris@cloudy.cs.uvic.ca:"\
-/home/chris/workspace/malware/scripts/dom0/load_dom0.sh \
-/home/chris/workspace/malware/scripts/dom0/refresh_dom0.sh \
-/home/chris/workspace/malware/drivers/xen/genshm-back/genshm-back.ko \
-/home/chris/workspace/malware/dom0/genshmb/main.py" .
+/home/chris/workspace/malware/trunk/scripts/dom0/load_dom0.sh \
+/home/chris/workspace/malware/trunk/scripts/dom0/refresh_dom0.sh \
+/home/chris/workspace/malware/trunk/drivers/xen/genshm-back/genshm-back.ko \
+/home/chris/workspace/malware/trunk/dom0/genshmb/main.py" .
 #sh load_dom0.sh
--- scripts/domU/refresh_domU.sh
+++ scripts/domU/refresh_domU.sh
@@ -2,9 +2,9 @@
 #/home/chris/workspace/malware/drivers/xen/malpage/malpage.ko \
 
 scp chris@cloudy.cs.uvic.ca:"\
-/home/chris/workspace/malware/scripts/domU/load_domU.sh \
-/home/chris/workspace/malware/scripts/domU/refresh_domU.sh \
-/home/chris/workspace/malware/drivers/xen/genshm-front/genshm-front.ko \
-/home/chris/workspace/malware/domU/c_target_sample/sample_proc \
-/home/chris/workspace/malware/domU/genshmf/main.py" .
+/home/chris/workspace/malware/trunk/scripts/domU/load_domU.sh \
+/home/chris/workspace/malware/trunk/scripts/domU/refresh_domU.sh \
+/home/chris/workspace/malware/trunk/drivers/xen/genshm-front/genshm-front.ko \
+/home/chris/workspace/malware/trunk/domU/c_target_sample/sample_proc \
+/home/chris/workspace/malware/trunk/domU/genshmf/main.py" .
 #sh load_domU.sh
